8fb82aee9b22ce586c637531cc81e73e5045321f
Ben Cornelis & Bob Ellis <student@epicodus.com>
2015-05-26 12:02:31 -0700

    refactor factorial to use recursion

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
 var factorial = function(number) {
   if (number === 0) {
     return 1;
+  } else {
+    return factorial(number - 1)*number
   }
-
-  var product = 1;
-  for (var multiplier = 1; multiplier <= number; multiplier += 1) {
-    product *= multiplier;
-  }
-  return product;
 };
